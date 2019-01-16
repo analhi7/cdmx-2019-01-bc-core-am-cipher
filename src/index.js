00a3e8097a814fe4eb9a0fc66d360fb22f24c8ee
analhi7 <analhi.e.m7@gmail.com>
2019-01-15 20:48:42 -0600

    agregue botones para cambiar de secciones usando none y block

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-const botonUno = document.getElementById('instrucciones');
-botonUno.addEventListener('click', botonA);
-function botonA(){
-    instrucciones.classList.add("hide");
-    inicio.classList.remove("hide");
 
+//Boton para mostrar instrucciones.
+
+instruB.onclick= function (){
+    document.getElementById('inicio').style.display= 'none'
+    document.getElementById('instrucciones').style.display= 'block'
+}
+
+//Boton para mostrar codificador
+comenzar.onclick= function (){
+    document.getElementById('inicio').style.display= 'none'
+
+    document.getElementById('codificador').style.display= 'block'
+}
+
+//Boton mostrar codificador desde instrucciones
+comenzarD.onclick= function (){
+    document.getElementById('instrucciones').style.display= 'none'
+
+    document.getElementById('codificador').style.display= 'block'
 }
