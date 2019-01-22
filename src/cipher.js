794440c6a3c718ecdd19ef46f1d27dd4bd6f980b
analhi7 <analhi-e-m7@gmail.com>
2019-01-22 16:19:05 -0600

    cipher minusculas

--- src/cipher.js
+++ src/cipher.js
@@ -4,36 +4,34 @@ window.cipher = {
 
     encode: (offset, usuarioMens) => {
 
-      //tener un mensaje
-      let mensaje = usuarioMens;
-
-      //Capturar el offset 
       let espacios = parseInt(offset);
-      //Cambiar a mayusculas
-      let mayus = mensaje.toUpperCase();
-     
-      //El lugar de la nueva palabra cifrada
-      let final = ''
-      //iterar la palabra
-      for (let i = 0; i < mayus.length; i++) {
-        //que la agarre 
-        let letra = mayus.charAt(i);
-        
-        //Identidicar Posición de caracter
-        //Encontrar el valor en el código ascii
-        let valor = letra.charCodeAt(0);
-        
-        //Poner la formula
-        let formula = (valor - 65 + espacios) % 26 + 65;
-        
-        //Obtener letra del cifrado
-        let resultado = String.fromCharCode(formula);
-        //Guardarlo en la variable final
-        final = final + resultado;
+  
+      let final = '';
+      
+      for (let i = 0; i < usuarioMens.length; i++) {
 
+        let valor = usuarioMens.charCodeAt(i);
+      
+
+        if(valor>= 65 && valor<=90){
+          let formula = (valor - 65 + espacios) % 26 + 65;
+          let resultado = String.fromCharCode(formula);
+          final = final + resultado;
+        }
+          else if ( valor >= 97 && valor<= 122) {
+          let minusculas = (valor - 97 + espacios) % 26 + 97;
+          let resultado = String.fromCharCode(minusculas);
+          final = final + resultado;
+    
+          } else{
+
+          let resultado = String.fromCharCode(valor);
+          final = final + resultado;
+          } 
       }
 
-      return final
+      return final;
+      
     },
 
 
